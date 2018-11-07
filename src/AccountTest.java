ea2fd0349121ec44fe6b5c0c35a26d5d3b7afc5c
Peter Bach <s185035@student.dtu.com>
2018-11-07 14:20:34 +0100

    Account test max score 3000

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -11,6 +11,9 @@ class AccountTest {
         test.addToScore(-5000);
         assertEquals(0,test.getScore());
 
+        Account test1 = new Account();
+        test1.addToScore(5000);
+        assertEquals(3000,test1.getScore());
 
     }
 
