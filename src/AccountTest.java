9f1aff3c4a7cc5f59a4c151f277f4756ae946409
Peter Bach <s185035@student.dtu.com>
2018-11-07 14:09:20 +0100

    Account test negativ score

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
-    Hej
 
     @Test
     void addToScore() {
+        Account test = new Account();
+        test.addToScore(-5000);
+        assertEquals(0,test.getScore());
+
+
     }
 
     @Test
