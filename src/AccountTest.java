4aa6681536345eb2bb4735321bf01cab8432da02
SaschaThorsgaardJacobsen <s171281@student.dtu.dk>
2018-11-07 11:50:53 +0100

    Oprettet test

---
+++ src/AccountTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void addToScore() {
+    }
+
+    @Test
+    void getScore() {
+    }
+}
---
+++ src/DiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @Test
+    void throwDice() {
+    }
+
+    @Test
+    void getEyes() {
+    }
+}
